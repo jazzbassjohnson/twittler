25274a50f6bb73e223f167114241eaeb4c6622fe
Gary Johnson <jazzbassjohnson@gmail.com>
2014-04-28 13:59:53 -0700

    added jquery.timeago.js and functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   function build(Username, Message, timeStamp){
-    return '<a href=""><img id="user_photo" src="unknown-person-48px.jpg"><span id="user_id">@'+Username+'</span></a><span id="timeStamp">'+timeStamp+'</span></br><span>'+Message+'</span>';
-  }
+      $("span.timeago").timeago()
 
+    return '<a href=""><img id="user_photo" src="unknown-person-48px.jpg"><span id="user_id">@'+Username+'</span></a><span id="timeStamp" class="timeago">'+$.timeago(timeStamp)+'</span></br><span>'+Message+'</span>';
+  }
   var lastestTweets;
   var $messageWindow = $('.messages');
 
@@ -15,16 +16,17 @@ $(document).ready(function(){
           var $tweet = $('<div class = "tweet_message"></div>');
           var photo = "<img id='user_photo' src='unknown-person-48px.jpg'>"
           // $tweet.html(photo + '@' + tweet.user.bold() + '</br> </br>' + tweet.message.italics());
-          $tweet.html(build(tweet.user, tweet.message, "Yesterday"));
+          $tweet.html(build(tweet.user, tweet.message, tweet.created_at));
           $tweet.appendTo($messageWindow);
           index -= 1;
+
         }
   }
-  window.username = prompt("What's your name?");
+  // window.username = prompt("What's your name?");
     populateTimeline();
     setInterval(populateTimeline, 5000);
 
-  streams.users[username] = [];
+  streams.users[window.username] = [];
     function postMessage(){
       var message = $('#field').val();
       $('#field').val('');
